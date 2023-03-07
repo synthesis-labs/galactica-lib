66838ec82ed5e3871408d83fd8c295dde9067877
Tom Wells <drshade@gmail.com>
2023-03-07 11:42:19 +0100

    first commit

---
+++ src/auth.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetTokenRequest {
+    pub code: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetTokenResponse {
+    pub token: DiscordAccessToken,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct DiscordAccessToken {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: u32,
+    pub refresh_token: String,
+    pub scope: String,
+}
---
+++ src/specs.rs
@@ -0,0 +1,45 @@
+use serde::{Deserialize, Serialize};
+
+use crate::auth::DiscordAccessToken;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct InstructionRequest {
+    pub token: DiscordAccessToken,
+    pub instruction: Instruction,
+    pub n: u32,
+    pub history: Vec<HistoryEntry>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Agent {
+    User,
+    Galactica,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct HistoryEntry {
+    pub agent: Agent,
+    pub content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct InstructionResponse {
+    pub content: Vec<String>,
+}
+
+type InstructionText = String;
+type InstructionReference = String;
+
+type EmailContent = String;
+type SnippetStart = usize;
+type SnippetLength = usize;
+type Tone = String;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Instruction {
+    GenerateCode(InstructionText),
+    GenerateCodeWithReference(InstructionText, InstructionReference),
+    Conversation(InstructionText),
+    ConversationWithReference(InstructionText, InstructionReference),
+    RetoneEmailParagraph(EmailContent, SnippetStart, SnippetLength, Tone),
+}
