c9def94d740b8ce50970f4da0a7563f1cc592eca
Tom Wells <drshade@gmail.com>
2023-03-13 18:53:22 +0100

    this time public

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,7 @@
 use serde::{de::DeserializeOwned, Serialize};
 
 #[derive(Serialize)]
-struct LogEntry<'r, REQ, RES>
+pub struct LogEntry<'r, REQ, RES>
 where
     REQ: Serialize + DeserializeOwned,
     RES: Serialize + DeserializeOwned,
