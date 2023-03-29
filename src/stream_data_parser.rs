d76ef836c94f244f0f91050071fa3745b5d3fee2
Tom Wells <drshade@gmail.com>
2023-03-29 18:45:00 +0200

    Supporting streams that can error

--- src/stream_data_parser.rs
+++ src/stream_data_parser.rs
@@ -1,10 +1,17 @@
+use nom::branch::alt;
 use nom::bytes::complete::{tag, take_until};
-use nom::character::complete::{multispace0, newline};
-use nom::combinator::opt;
-use nom::multi::many0;
+use nom::character::complete::{alpha1, multispace0, newline};
+use nom::combinator::{eof, not, opt, rest};
+use nom::multi::{many0, many1};
 use nom::sequence::tuple;
 
 use crate::parser::Parser;
+
+#[derive(Debug)]
+pub enum StreamRecordType {
+    Data(Vec<String>),
+    Error(String),
+}
 
 fn single_data_record() -> impl Fn(&str) -> Parser<&str, String> {
     move |i| {
@@ -19,14 +26,28 @@ fn blank_data_record() -> impl Fn(&str) -> Parser<&str, ()> {
     move |i| tuple((tag(":"), newline))(i).map(|(next, _)| (next, ()))
 }
 
-pub fn stream_data_parser() -> impl Fn(&str) -> Parser<&str, Vec<String>> {
+pub fn stream_data_parser() -> impl Fn(&str) -> Parser<&str, StreamRecordType> {
     move |i| {
         tuple((
             opt(newline),
             opt(blank_data_record()),
             many0(single_data_record()),
         ))(i)
-        .map(|(n, (_, _, v))| (n, v))
+        .map(|(n, (_, _, v))| (n, StreamRecordType::Data(v)))
+    }
+}
+
+pub fn error_parser() -> impl Fn(&str) -> Parser<&str, StreamRecordType> {
+    move |i| {
+        tuple((tag("{"), rest))(i)
+            .map(|(n, (v, rem))| (n, StreamRecordType::Error(format!("{}{}", v, rem))))
+    }
+}
+
+pub fn stream_data_or_error_parser() -> impl Fn(&str) -> Parser<&str, StreamRecordType> {
+    move |i| {
+        let x = alt((error_parser(), stream_data_parser()))(i).map(|(n, v)| (n, v));
+        x
     }
 }
 
@@ -39,12 +60,40 @@ mod tests {
     #[test]
     pub fn test_parser() {
         let input1 = "data: {\"some_json\":\"blahx\"}\n";
-        println!("Input1 => {:?}", parse(stream_data_parser(), input1));
+        println!(
+            "Input1 => {:?}",
+            parse(stream_data_or_error_parser(), input1)
+        );
 
         let input2 = "data: {\"some_json\":\"blah1\"}\ndata: {\"some_json\":\"blah2\"}\n";
-        println!("Input2 => {:?}", parse(stream_data_parser(), input2));
+        println!(
+            "Input2 => {:?}",
+            parse(stream_data_or_error_parser(), input2)
+        );
 
         let input3 = ":\n";
-        println!("Input3 => {:?}", parse(stream_data_parser(), input3));
+        println!(
+            "Input3 => {:?}",
+            parse(stream_data_or_error_parser(), input3)
+        );
+
+        let input4 = "{\n\
+            \"error\": {\n\
+                \"message\": \"You exceeded your current quota, please check your plan and billing details.\",\n\
+                \"type\": \"insufficient_quota\",\n\
+                \"param\": null,\n\
+                \"code\": null\n\
+            }\n\
+        }\n";
+        println!(
+            "Input4 => {:?}",
+            parse(stream_data_or_error_parser(), input4)
+        );
+
+        let input5 = "{bob}";
+        println!(
+            "Input5 => {:?}",
+            parse(stream_data_or_error_parser(), input5)
+        );
     }
 }
