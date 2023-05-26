81cc3c55c1258cf6d01df8dfcd6271bca320d036
Tom Wells <drshade@gmail.com>
2023-05-26 09:57:13 +0200

    added user entry to log

--- src/log.rs
+++ src/log.rs
@@ -1,11 +1,13 @@
 use serde::{de::DeserializeOwned, Serialize};
 
 #[derive(Serialize)]
-pub struct LogEntry<'r, REQ, RES>
+pub struct LogEntry<'r, USER, REQ, RES>
 where
+    USER: Serialize + DeserializeOwned,
     REQ: Serialize + DeserializeOwned,
     RES: Serialize + DeserializeOwned,
 {
+    pub user: Option<&'r USER>,
     pub request: &'r REQ,
     pub response: &'r RES,
 }
